e2fe1fa5ddc3aa9ddb5aeb58452d6de1feb2f311
Wurielle <noahfenghom@gmail.com>
2019-05-13 21:45:03 +0200

    Added pristine

---
+++ config/storybook/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const webpack = require('webpack');
+
+const DSSPlugin = require('@bit/wurielle.pristine.webpack.dss-plugin');
+const JsonSassPlugin = require('@bit/wurielle.pristine.webpack.json-sass-plugin');
+module.exports = {
+    resolve: {
+        alias: {
+            '@config':  path.resolve(__dirname, '../../config'),
+            '@':  path.resolve(__dirname, '../../src'),
+        }
+    },
+    plugins:[
+        new DSSPlugin({
+            filter: /\.s(c|a)ss/,
+            output: './src/styleguide.json',
+            watch: './src'
+        }),
+        new JsonSassPlugin('./config/theme.js', './config/theme.scss'),
+    ]
+};
---
+++ vue.config.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+const argv = require('minimist')(process.argv.slice(2));
+const pkg = require('./package.json');
+const JsonSassPlugin = require('@bit/wurielle.pristine.webpack.json-sass-plugin');
+
+module.exports = {
+    publicPath: '/',
+    chainWebpack: config => {
+        // vue inspect --plugins
+        config
+            .entry('app')
+            .add('animate.css/animate.min.css')
+            .add('ionicons/dist/css/ionicons.min.css')
+        ;
+    },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                '@config':  path.resolve(__dirname, 'config'),
+                '@components':  path.resolve(__dirname, 'components'),
+            }
+        },
+        plugins:[
+            new webpack.DefinePlugin({
+                // 'API_AUTH_USERNAME': JSON.stringify(argv.apiAuthUsername.trim()),
+                // 'API_AUTH_PASSWORD': JSON.stringify(argv.apiAuthPassword.trim()),
+                // 'DOMAIN_URL': JSON.stringify(argv.domain.trim()),
+                // ...
+            }),
+            new JsonSassPlugin('./config/theme.js', './config/theme.scss'),
+        ]
+    },
+    pluginOptions: {
+        electronBuilder: {
+            mainProcessFile: 'src/.electron/main.ts',
+            builderOptions: {
+                appId: `com.${pkg.name}.app`,
+                files: [
+                    '**/*',
+                    '!src',
+                    '!dist',
+                    '!server',
+                    '!pristine',
+                ],
+                mac: {
+                    'category': `com.${pkg.name}.app`
+                },
+                win: {},
+                publish: [
+                    argv.c === 'store' ?
+                        {
+                            provider: 's3',
+                            endpoint: 'http://55.55.55.1:9000',
+                            bucket: pkg.name,
+                        } :
+                        {
+                            // Generic
+                            provider: 'generic',
+                            url: 'https://path.to/folder-containing/lastestdotyml',
+
+                            // GitHub e.g: https://github.com/MyGitHubUsername/my-repo-name
+                            // provider: "github",
+                            // owner: "MyGitHubUsername",
+                            // repo: "my-repo-name"
+                        }
+                ]
+            }
+        }
+    }
+};
